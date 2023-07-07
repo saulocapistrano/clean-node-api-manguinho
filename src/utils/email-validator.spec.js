d86e91994a24e2c7fdfc68c04c4726165c041907
saulocapistrano <saulocapistrano@gmail.com>
2023-07-07 11:33:14 -0300

    ensure email validator call email validator is correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -18,12 +18,16 @@ describe('Email Validator', () => {
         expect(isEmailValid).toBe(true)
     })
     
-describe('Email Validator', () => {
     test('Should return false if validator returns false',() => {
         validator.isEmailValid = false
         const sut = makeSut()
         const isEmailValid = sut.isValid('invalid_email@mail.com')
         expect(isEmailValid).toBe(false)
     })
-})
+    
+    test('Should call validator with correct email',() => {
+        const sut = makeSut()
+        sut.isValid('any_email@mail.com')
+        expect(validator.email).toBe('any_email@mail.com')
+    })
 })
