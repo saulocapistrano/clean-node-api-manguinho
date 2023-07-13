d9d4d1e121c268aeccdeba576626cf61b35e0b2f
saulocapistrano <saulocapistrano@gmail.com>
2023-07-13 13:31:21 -0300

    send

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -13,6 +13,17 @@ const makeEncripter = ()=>{
     encrypterSpy.isValid = true
     return encrypterSpy
 }
+const makeTokenGenerator = ()=>{
+    class TokenGeneratorSpy{
+        async generate(userId){
+            this.userId = userId
+            return this.accessToken
+        }
+    }
+    const tokenGeneratorSpy = new TokenGeneratorSpy()
+    tokenGeneratorSpy.accessToken = "any_token"
+    return tokenGeneratorSpy
+}
 
 const makeLoadUserByEmailRepositorySpy = ()=>{
     class LoadUserByEmailRepositorySpy {
@@ -23,6 +34,7 @@ const makeLoadUserByEmailRepositorySpy = ()=>{
     } 
     const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
     loadUserByEmailRepositorySpy.user = {
+        id: 'any_id',
         password:'hashed_password'
     }
     return loadUserByEmailRepositorySpy
@@ -31,11 +43,13 @@ const makeLoadUserByEmailRepositorySpy = ()=>{
 const makeSut = () => {
     const encrypterSpy = makeEncripter()
     const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepositorySpy()
-    const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
+    const tokenGeneratorSpy = makeTokenGenerator()
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
     return {
         sut,
         loadUserByEmailRepositorySpy, 
-        encrypterSpy
+        encrypterSpy,
+        tokenGeneratorSpy
     }
     
 }
@@ -90,6 +104,13 @@ describe('Auth UseCase', ()=>{
         await sut.auth('valid_email@mail.com','any_password')
         expect(encrypterSpy.password).toBe('any_password')
         expect(encrypterSpy.hashedPassword).toBe(loadUserByEmailRepositorySpy.user.password)
+    })
+    
+    test('Should call TokenGenerator with correct userId', async () => {
+        const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut()
+        await sut.auth('valid_email@mail.com','valid_password')
+        expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
+       
     })
   
 
